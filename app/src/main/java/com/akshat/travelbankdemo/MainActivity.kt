74342ee1e56c13e70f8cc1f9f2ce9a8ca88e85da
Akshat <akshat@akshats-mbp.home>
2022-11-04 14:26:52 -0400

    Initial Commit for App skeleton

---
+++ app/src/main/java/com/akshat/travelbankdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.akshat.travelbankdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
